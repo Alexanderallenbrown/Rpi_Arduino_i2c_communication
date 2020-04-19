0002c977f33dd2f1be9972318bf30a96aaa02bd8
Alexander Brown <brownaa@lafayette.edu>
2020-04-19 16:47:35 -0400

    new mapping

--- i2cthinger/i2cthinger.ino
+++ i2cthinger/i2cthinger.ino
@@ -16,7 +16,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(analogRead(0));
+Serial.print(analogRead(0));
+Serial.print("\t");
+Serial.print(analogRead(1));
+Serial.print("\t");
+Serial.print(analogRead(2));
+Serial.print("\t");
+Serial.print(analogRead(3));
+Serial.print("\t");
+Serial.println();
 delay(1);
 }
 
